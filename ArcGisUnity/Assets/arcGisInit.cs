5ce5dac0c5ad37a27354e6b9386ae826b8f64b2e
navintc <navin.thamindu@gmail.com>
2023-05-01 12:14:52 +0530

    Adding arcGis and a camera controller

---
+++ ArcGisUnity/Assets/arcGisInit.cs
@@ -0,0 +1,27 @@
+using Esri.ArcGISMapsSDK.Components;
+using Esri.ArcGISMapsSDK.Samples.Components;
+using Esri.ArcGISMapsSDK.Utils.GeoCoord;
+using Esri.GameEngine.Extent;
+using Esri.GameEngine.Geometry;
+using Esri.Unity;
+using Unity.Mathematics;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using System;
+[ExecuteAlways]
+public class arcGisInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
